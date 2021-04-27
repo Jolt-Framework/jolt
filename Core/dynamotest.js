164206bc731ee6fcbefec21498ccf1f565f4c9ea
Rodney Matambo <rmatambo8@icloud.com>
2021-04-27 18:37:04 -0400

    all working

--- Core/dynamotest.js
+++ Core/dynamotest.js
@@ -3,29 +3,30 @@ const run = async () => {
   try {
     const uuidv4 = require("uuid").v4()
     const db = new Dynamo()
-  
-    console.log('creating dynamo table');
-
-    // const table = await db.createTable("CoreJamstack" + uuidv4);
-    console.log('adding items');
-    const items = await db.addItems({
+    const tableName = "CoreJamstack1"
+    // console.log('creating dynamo table');
+    // const table = await db.createTable(tableName);
+    // console.log('adding items');
+    // const items = await db.getItems(tableName, "testApp2", "new-bucket-etwesfchdyx1");
+    const items = await db.addItems(tableName, {
       lambdas: [
         'arn:aws:lambda:us-east-1:886346126803:function:createNote:14',
         'arn:aws:lambda:us-east-1:886346126803:function:deleteNote:14',
-        // 'arn:aws:lambda:us-east-1:886346126803:function:getNotes:14',
+        'arn:aws:lambda:us-east-1:886346126803:function:getNotes:14',
         'arn:aws:lambda:us-east-1:886346126803:function:updateNote:14',
         'arn:aws:lambda:us-east-1:886346126803:function:edge-proxy:10'
       ],
       deployed: true,
       region: 'us-east-1',
-      projectName: "testApp",
+      projectName: "testApp1",
       bucketName: 'new-bucket-etwesfchdyx1',
       api: {
         apiName: 'testName',
         url: 'https://7yki6ogr7j.execute-api.us-east-1.amazonaws.com',
-        apiId: '7yki6ogr7j'
+        // apiId: '7yki6ogr7j'
+        apiId: '8yki6ogr7j'
       },
-      cloudfrontId: 'E1DI8SOZHQDW21',
+      cloudfrontId: 'E1DI8SOZHQDW1',
       domainName: 'd3lud355m027lp.cloudfront.net'
     })
     
