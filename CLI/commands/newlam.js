1a6a0fffe3d759c6f0607a2d3d7209460b81ca1c
Owen Lenz <owen.lenz@gmail.com>
2021-05-04 16:40:35 -0400

    Rewording of the new lambda template

--- CLI/commands/newlam.js
+++ CLI/commands/newlam.js
@@ -3,16 +3,21 @@ const fs = require("fs");
 const path = require("path");
 
 const lambdaTemplate =
-`// Template for a typical lambda handler.
+`// This is a generic template for a lambda function
 
 // Note: This is the asynchronous variety of Lambda.
 //       Asynchronous processes within the function should use await syntax or promises.
-//       For async processes that use the event loop (such as with setTimeout), you can use the the callback function parameter to return once the process has completed
+//       For async processes that use the event loop (such as with setTimeout),
+//       you can use the the callback function parameter to return once the process has completed
 
 exports.handler = async (event, context, callback) => {
 
+
   // Function logic here
 
+
+  // Function return value.
+  // Note: body property must be valid JSON and should be stringified before it is returned
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -20,7 +25,7 @@ exports.handler = async (event, context, callback) => {
     })
   }
 }
-`
+`;
 
 const newlam = () => {
   const { functionsFolder } = config.buildInfo;
